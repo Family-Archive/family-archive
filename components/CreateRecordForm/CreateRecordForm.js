37dad4b136fe7445346f8eea6244ce45bf2b3b7e
Sam <sam.smucker@gmail.com>
2023-06-02 21:50:43 -0500

    quick proof of concept for loading react components as form elements (somewhat) dynamically

--- components/CreateRecordForm/CreateRecordForm.js
+++ components/CreateRecordForm/CreateRecordForm.js
@@ -1,19 +1,27 @@
 import styles from './CreateRecordForm.module.scss'
+import fieldComponents from './FieldComponents.js'
 
 const CreateRecordForm = (props) => {
     return (
         <form method="POST" action={`/api/record/${props.recordTypeData.type}`} className={styles.CreateRecordForm}>
-            {props.recordTypeData.fields.map(field => {
+            {props.recordTypeData.fields.map((field, index) => {
                 let Element
-                switch (field.type) {
-                    case "textarea":
-                        Element = "textarea"
-                        break
-                    default:
-                        Element = "input"
+
+                // If first letter of field type is uppercase it is a component.
+                const firstLetter = Array.from(field.type)[0];
+                if (firstLetter === firstLetter.toUpperCase()) {
+                    Element = fieldComponents[field.type];
+                } else {
+                    switch (field.type) {
+                        case "textarea":
+                            Element = "textarea"
+                            break
+                        default:
+                            Element = "input"
+                    }
                 }
-                return <formitem>
-                    <label for={field.name}>{field.name}</label>
+                return <formitem key={index}>
+                    <label htmlFor={field.name}>{field.name}</label>
                     <Element id={field.name} name={field.name} type={field.type} />
                 </formitem>
             })}
---
+++ components/CreateRecordForm/FieldComponents/PersonSelector.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PersonSelector() {
+    return (
+        <>
+            <div>PersonSelectorComponent</div>
+            <input type="text"></input>
+        </>
+    )
+}
