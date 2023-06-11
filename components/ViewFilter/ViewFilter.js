28ecff7747a35aaf2f0a827ed403dd25f0474798
Limekiller <me@bryceyoder.com>
2023-06-11 16:47:08 -0400

    Begin working on filters

--- components/ViewFilter/ViewFilter.js
+++ components/ViewFilter/ViewFilter.js
@@ -4,14 +4,20 @@ import { useState, useEffect } from 'react'
 import { useRouter, usePathname } from 'next/navigation'
 import styles from './ViewFilter.module.scss'
 import SortToggle from './SortToggle/SortToggle/SortToggle'
+import FilterSetting from './FilterSetting/FilterSetting'
 
 const ViewFilter = (props) => {
     const router = useRouter();
     const pathname = usePathname()
 
     const [sortSettings, setsortSettings] = useState({
-        option: props.params.sort ? props.params.sort : "name",
-        direction: props.params.dir ? props.params.dir : "asc"
+        option: "",
+        direction: ""
+    })
+
+    const [filters, setfilters] = useState({
+        "name": [],
+        "age": []
     })
 
     const updateSortSettings = (settings) => {
@@ -21,11 +27,16 @@ const ViewFilter = (props) => {
         })
     }
 
+    const addFilter = (name, entry) => {
+        let entryArray = filters[name]
+        entryArray.push(entry)
+        setfilters({ ...filters, [name]: entryArray })
+    }
+
     useEffect(() => {
         router.replace(`${pathname}?sort=${sortSettings.option}&dir=${sortSettings.direction}`);
         router.refresh()
-    }, [sortSettings])
-
+    }, [sortSettings, filters])
 
     return (
         <div className={styles.ViewFilter}>
@@ -37,6 +48,9 @@ const ViewFilter = (props) => {
 
             <section className={styles.filterOptions}>
                 <span className={styles.sectionHead}>Filter by</span>
+                {Object.keys(filters).map(filter => {
+                    return <FilterSetting name={filter} key={filter} entries={filters[filter]} addFilter={addFilter} />
+                })}
             </section>
         </div>
     )
