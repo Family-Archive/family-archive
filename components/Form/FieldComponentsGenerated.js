dd5c57931005e3680479989bf08f43f5590f3cac
Sam <sam.smucker@gmail.com>
2023-07-03 15:18:02 -0500

    add script to auto-generate components file and add APPLICATION_ROOT env variable

---
+++ components/Form/FieldComponentsGenerated.js
@@ -0,0 +1,7 @@
+import FileInput from './FieldComponents/FileInput/FileInput'
+import PersonSelector from './FieldComponents/PersonSelector/PersonSelector'
+
+export default {
+	FileInput: FileInput,
+	PersonSelector: PersonSelector
+}
