b56e441735f9bf2fef913a3e53d21f905cafe089
Limekiller <me@bryceyoder.com>
2023-07-08 15:32:43 -0400

    Create record view page

--- app/(dashboard)/record/[id]/page.js
+++ app/(dashboard)/record/[id]/page.js
@@ -1,5 +1,7 @@
 import { cookies } from 'next/dist/client/components/headers'
 import lib from '../../../../lib/lib'
+import { prisma } from "../../../db/prisma"
+import Image from 'next/image'
 
 import styles from './ViewRecord.module.scss'
 import BreadcrumbTrail from '@/components/BreadcrumbTrail/BreadcrumbTrail'
@@ -17,7 +19,18 @@ const ViewRecord = async ({ params, searchParams }) => {
     const recordData = await fetchRecord(params);
     const record = recordData.data.record
 
-    console.log(recordData.data.fields)
+    // Fetch people connected to record by reading the custom "Person" field
+    let people = []
+    for (let field of recordData.data.fields) {
+        if (field.name === "Person") {
+            for (let id of JSON.parse(field.value)) {
+                const person = await prisma.person.findFirst({
+                    where: { id: id }
+                })
+                people.push(person)
+            }
+        }
+    }
 
     return (
         <div className={styles.ViewRecord}>
@@ -26,29 +39,53 @@ const ViewRecord = async ({ params, searchParams }) => {
                 <span className={styles.type}>{record.type}</span>
                 <BreadcrumbTrail name={record.name} />
             </div>
+
             <div className={styles.mainContent}>
-                <div className={styles.file} />
-                <div className={styles.info}>
-                    <strong>Description</strong>
-                    <p>{record.description}</p>
-                    {recordData.data.fields.map(field => {
-                        return <div key={field.id}>
-                            <strong>{field.name}</strong>
-                            <p>{field.value}</p>
+
+                <div className={styles.content}>
+                    <div className={styles.file}>
+                        <Image src={`/api/file/${recordData.data.files[0].id}`} width={500} height={500} />
+                    </div>
+                    <div className={styles.info}>
+                        <strong>Description</strong>
+                        <p>{record.description}</p>
+
+                        {recordData.data.fields.map(field => {
+                            return <div key={field.id}>
+                                <strong>{field.name}</strong>
+                                <p>{field.value}</p>
+                            </div>
+                        })}
+
+                        <div className={styles.people}>
+                            <span className={`${styles.icon} material-icons`}>boy</span>
+                            {people.map(person => {
+                                return <button className={styles.person}>
+                                    {person.fullName}
+                                </button>
+                            })}
                         </div>
-                    })}
+                    </div>
                 </div>
+
                 <div className={styles.otherInfo}>
-                    <h2>Details</h2>
-                    <strong>Created by</strong>
-                    <p>{record.userCreatedId}</p>
-                    <strong>Created at</strong>
-                    <p>{record.createdAt}</p>
-                    <strong>Last updated</strong>
-                    <p>{record.updatedAt}</p>
-                    <strong>Family ID</strong>
-                    <p>{record.familyId}</p>
+                    <div>
+                        <h2>Details</h2>
+                        <strong>Created by</strong>
+                        <p>{record.userCreatedId}</p>
+                        <strong>Created at</strong>
+                        <p>{record.createdAt}</p>
+                        <strong>Last updated</strong>
+                        <p>{record.updatedAt}</p>
+                        <strong>Type</strong>
+                        <p>{record.type}</p>
+                    </div>
+                    <div>
+                        <strong>Family ID</strong>
+                        <p>{record.familyId}</p>
+                    </div>
                 </div>
+
             </div>
         </div>
     )
